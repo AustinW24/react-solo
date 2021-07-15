60070bff5e72f024078054fc46cec0f6c76db8f2
Austin Williams <austinwill264@gmail.com>
2021-07-15 14:49:36 -0700

    testing user authentication

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -4,4 +4,10 @@ router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+
 module.exports = router;
