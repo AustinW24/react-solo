c499fd844d9e0c2de2ebf1b163e00a03534866eb
Austin Williams <austinwill264@gmail.com>
2021-07-16 10:27:56 -0700

    sessions action/reducer

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -6,7 +6,7 @@ import configureStore from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-
+import * as sessionActions from './store/session';
 // ... const store = configureStore();
 const store = configureStore();
 
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
