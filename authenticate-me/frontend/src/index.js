d8ac75b325526d1085dbf91cbc228ea840a6fdc1
Austin Williams <austinwill264@gmail.com>
2021-07-16 10:02:13 -0700

    frontend setup complete

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -2,22 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './index.css';
 import configureStore from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+// ... const store = configureStore();
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
