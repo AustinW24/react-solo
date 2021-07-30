9cdca2532bc1aa6776d02d1101ff6e68ffa3d1d2
Austin Williams <austinwill264@gmail.com>
2021-07-30 08:54:36 -0700

    onChange

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -2,6 +2,7 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const router = express.Router();
 const { Song } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth');
 
 
 router.get('/home', asyncHandler(async function (req, res) {
@@ -25,11 +26,16 @@ router.put('/:id', asyncHandler(async function (req, res) {
     return res.json(updatedSong);
 }));
 
-router.delete('/home', asyncHandler(async function (req, res) {
-    const songId = parseInt(req.params.id)
+router.delete('/home', restoreUser, asyncHandler(async function (req, res) {
+    const { id } = req.params
     // const song = await Song.findByPk(songId);
-    const deletedSong = await song.destroy({where: {id: songId}});
-    return res.json(deletedSong)
+    const song = await Song.findByPk(id);
+    await song.destroy();
+    if(!song) {
+        res.json({ message: "successfully deleted song"})
+    } else {
+        res.json({message:"Cannot delete song."});
+    }
 }));
 
 router.post('/:id/edit', asyncHandler(async function (req, res) {
